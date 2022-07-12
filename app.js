b8009d27575b4ffdf43a0282fb0e87ee1778aee3
Tulasi <lakshmitulasitellakula@gmail.com>
2022-07-12 22:33:37 +0530

    second commit12

--- app.js
+++ app.js
@@ -33,6 +33,20 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 
+if ( process.env.NODE_ENV === "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
+}
 
 // routes
 
@@ -50,20 +64,7 @@ app.use(function (err,req,res,next){
 
  
 
-if ( process.env.NODE_ENV === "production"){
 
-    app.use(express.static("client/build"));
-
-    const path = require("path");
-
-    app.get("*", (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-
-
-}
 
 app.listen(port,()=>{
     console.log("Server started");
