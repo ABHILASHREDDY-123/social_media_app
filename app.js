3d5958ee3e05fe1925b4ba95bcc5b3948a800ad9
= <=>
2023-09-09 00:24:32 +0530

    added backend

--- app.js
+++ app.js
@@ -48,20 +48,20 @@ app.use(function (err,req,res,next){
 // server listening
 
 
-if ( process.env.NODE_ENV === "production"){
+// if ( process.env.NODE_ENV === "production"){
 
-    app.use(express.static("client/build"));
+//     app.use(express.static("client/build"));
 
-    const path = require("path");
+//     const path = require("path");
 
-    app.get("*", (req, res) => {
+//     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 
-    })
+//     })
 
 
-}
+// }
  
 
 
--- client/src/LoadingComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
- 
-const Load = () => (
-    <ReactLoading type="spin" color="white" height={'50px'} width={'50px'} />
-);
- 
-export default Load;
